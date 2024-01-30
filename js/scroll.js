3caedb22affa1e21663f692190a6977fde627274
facuandrean <facundoandrean22@gmail.com>
2024-01-30 12:11:55 -0300

    Vista Mobile V1.0.0

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,26 @@
 export function scroll() {
     // Script para restablecer la posición de desplazamiento al recargar la página
+
+    // Función para restablecer la posición de desplazamiento al principio de la página
+    function resetScrollPosition() {
+        window.scrollTo(0, 0);
+    }
+
+    // Función para restablecer la posición de desplazamiento al principio de la sección
+    function resetScrollSection() {
+        document.querySelector(window.location.hash).scrollIntoView();
+    }
+
+    // Evento beforeunload que se dispara antes de recargar la página
+    window.addEventListener("beforeunload", resetScrollSection);
     
     // Verifica si hay un fragmento de URL y si coincide con el ID de una sección
     if (window.location.hash && document.querySelector(window.location.hash)) {
         // Desplázate a la sección correspondiente
-        document.querySelector(window.location.hash).scrollIntoView();
+        resetScrollSection();
     } else {
         // Si no hay fragmento de URL o no coincide con ninguna sección, desplázate al inicio
-        window.scrollTo(0, 0);
+        resetScrollPosition();
     }
-    
-    
+   
 }
