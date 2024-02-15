81f3ac26b7b8a8cc7ddde8a16da4af3097cc5dbf
facuandrean <facundoandrean22@gmail.com>
2024-02-15 20:37:57 -0300

    Versión v1.0.2 tablet terminada

---
+++ js/main_job.js
@@ -0,0 +1,9 @@
+import { btnNav } from './btn-nav.js';
+import { headerJob } from './header_job.js';
+import { nav } from './nav.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    headerJob();
+    btnNav();
+    nav();
+})
