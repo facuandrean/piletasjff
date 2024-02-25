4a956026fe0f526d70d1d7a3b109b323fb7f87e3
facuandrean <facundoandrean22@gmail.com>
2024-02-24 22:58:37 -0300

    Versión de prueba en pagina de trabajos

--- js/main_job.js
+++ js/main_job.js
@@ -1,9 +1,11 @@
 import { btnNav } from './btn-nav.js';
-import { headerJob } from './header_job.js';
+// import { headerJob } from './header_job.js';
+import { header } from './header.js';
 import { nav } from './nav.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    headerJob();
+    // headerJob();}
+    header();
     btnNav();
     nav();
 })
