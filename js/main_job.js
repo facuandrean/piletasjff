c36c605e34ee310c704e3da16cd3d9ef99108114
facuandrean <facundoandrean22@gmail.com>
2024-02-25 17:52:23 -0300

    Commit de prueba

--- js/main_job.js
+++ js/main_job.js
@@ -1,10 +1,8 @@
 import { btnNav } from './btn-nav.js';
-// import { headerJob } from './header_job.js';
 import { header } from './header.js';
 import { nav } from './nav.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // headerJob();}
     header();
     btnNav();
     nav();
